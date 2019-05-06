951f6d6f2db3b45c48807a42211f557c6cb65a42
sEver <sEver@users.noreply.github.com>
2019-05-06 05:09:28 +0200

    Modularization (#12)
    * WSAD movement + click & go
    
    Click and go commented out in js and css file
    
    * del spaces
    
    * add node server
    
    * Removing node modules from repo
    
    * Add .gitingore
    
    * webpack support
    
    * Add first module
    
    * Add rocket model
    
    * Add rocket logic

--- main.js
+++ main.js
@@ -1,40 +1,42 @@
 /* eslint-env es6 */
-
+import rocket from './rocket';
 // WSAD movemant
+
 const gameWrapper = document.querySelector("#content-container"),
 	  rocketWrapper = document.getElementById("rocket-wrapper");
 
 window.addEventListener('keydown', e => {
-	
+
 	let offsets = rocketWrapper.getBoundingClientRect();
-		
-		if(e.keyCode === 68 ) {
-			let xPos = offsets.left;
-			rocketWrapper.style.left = xPos+ 20 + 'px';
+  
+  const delta = 1;
+		if(e.keyCode === 68 ) { // D - right
+			rocket.vel.x = rocket.vel.x + delta;
 		}
-		else if(e.keyCode === 83) {
-			let yPos = offsets.top;
-			rocketWrapper.style.top = yPos + 20 + 'px';
+		else if(e.keyCode === 83) {// S - down
+			rocket.vel.y = rocket.vel.y + delta;
 		}
-		else if(e.keyCode === 87 && offsets.top-20 >= 0) {
-			let yPos = offsets.top;
-			rocketWrapper.style.top = yPos - 20 + 'px';
+		else if(e.keyCode === 87 && offsets.top-delta >= 0) {// W - up
+			rocket.vel.y = rocket.vel.y - delta;
 		}
-		else if(e.keyCode === 65 && offsets.left-20 >= 0) {
-			let xPos = offsets.left;
-			rocketWrapper.style.left = xPos - 20 + 'px';
+		else if(e.keyCode === 65 && offsets.left-delta >= 0) {// A - left
+			rocket.vel.x = rocket.vel.x - delta;
 		}
+
+		console.log(rocket);
 });
 
-/*
-// click and move code ->
-gameWrapper.addEventListener("click", e => {
-	
-	let xPosition = e.clientX - gameWrapper.getBoundingClientRect().left - (rocketWrapper.clientWidth / 2);
-	
-	let yPosition = e.clientY - gameWrapper.getBoundingClientRect().top - (rocketWrapper.clientHeight / 2);
-	// in case of a wide border, the boarder-width needs to be considered in the formula above
-	rocketWrapper.style.left = xPosition + "px";
-	rocketWrapper.style.top = yPosition + "px";
-});
-*/
+function updateView() {
+  rocket.updateRocketState();
+	updateRocketOnScreen();
+	window.requestAnimationFrame(updateView);
+}
+//setInterval(updateView, 10);
+window.requestAnimationFrame(updateView);
+console.log(rocket);
+
+function updateRocketOnScreen() {
+	rocketWrapper.style.left = rocket.pos.x + 'px';
+	rocketWrapper.style.top = rocket.pos.y + 'px';
+  rocketWrapper.style.transform = `rotate(${rocket.angle % 360}deg)`;
+};
