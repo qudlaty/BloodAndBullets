727b658a436cc87de48c246a19ef4ab375e6389c
qudlaty <qudlaty@users.noreply.github.com>
2019-05-06 04:32:16 +0200

    Add rocket logic

--- main.js
+++ main.js
@@ -9,27 +9,38 @@ const gameWrapper = document.querySelector("#content-container"),
 window.addEventListener('keydown', e => {
 
 	let offsets = rocketWrapper.getBoundingClientRect();
-
+  
+  const delta = 1;
 		if(e.keyCode === 68 ) { // D - right
-			rocket.pos.x = rocket.pos.x + 20;
+			rocket.vel.x = rocket.vel.x + delta;
 		}
 		else if(e.keyCode === 83) {// S - down
-			rocket.pos.y = rocket.pos.y + 20;
+			rocket.vel.y = rocket.vel.y + delta;
 		}
-		else if(e.keyCode === 87 && offsets.top-20 >= 0) {// W - up
-			rocket.pos.y = rocket.pos.y - 20;
+		else if(e.keyCode === 87 && offsets.top-delta >= 0) {// W - up
+			rocket.vel.y = rocket.vel.y - delta;
 		}
-		else if(e.keyCode === 65 && offsets.left-20 >= 0) {// A - left
-			rocket.pos.x = rocket.pos.x - 20;
+		else if(e.keyCode === 65 && offsets.left-delta >= 0) {// A - left
+			rocket.vel.x = rocket.vel.x - delta;
 		}
 
-		updateRocketOnScreen();
 		console.log(rocket);
 });
+
+
+function updateView() {
+  rocket.updateRocketState();
+	updateRocketOnScreen();
+	window.requestAnimationFrame(updateView);
+}
+//setInterval(updateView, 10);
+window.requestAnimationFrame(updateView);
+console.log(rocket);
 
 function updateRocketOnScreen() {
 	rocketWrapper.style.left = rocket.pos.x + 'px';
 	rocketWrapper.style.top = rocket.pos.y + 'px';
+  rocketWrapper.style.transform = `rotate(${rocket.angle % 360}deg)`;
 };
 
 console.log(rocket.name);
--- rocket.js
+++ rocket.js
@@ -3,6 +3,39 @@ var rocket = {
   pos: {
     x: 100,
     y: 100
+  },
+  vel: {
+    x: 0,
+    y: 0
+  },
+  angle: 0,
+  
+  calcNewPos: function(){
+    let friction = 0.99;
+    this.pos.x = this.pos.x + this.vel.x;
+    this.pos.y = this.pos.y + this.vel.y;
+    this.vel.x = this.vel.x * friction;
+    this.vel.y = this.vel.y * friction;
+  },
+  
+  calcNewAangle: function(){
+      if(this.vel.y >= 0) {
+        this.angle = - Math.atan(
+          this.vel.x/this.vel.y
+        ) * (180/Math.PI); 1     
+      }else if(this.vel.y < 0) {
+        this.angle = (180/Math.PI) * 
+          (
+            Math.atan(
+              this.vel.x/-this.vel.y
+            ) + Math.PI
+          )
+      }
+  },
+  
+  updateRocketState: function(){
+    this.calcNewAangle();
+    this.calcNewPos();
   }
 }
 
