eab8b42e970d3bb10a5ddb9da12e44c1e96881b9
sEver <sEver@users.noreply.github.com>
2019-09-18 00:58:29 +0200

    Enhance EntityView and its styles

--- src/components/EntityView.js
+++ src/components/EntityView.js
@@ -3,12 +3,20 @@ import './EntityView.css';
 
 export default function EntityView(props) {
   return (
-    <div
-      className="entity"
-    >
-      <strong>{props.entity.name}</strong>
-      <em> Aged: {props.entity.age} </em><br />
-      <progress max="100" value={props.entity.hp}>
+    <div className="entity">
+      <div className="position" title="Position">
+        <span>[{props.entity.position.x}, {props.entity.position.y}]</span>
+      </div>
+
+      <strong title="Name">{props.entity.name}</strong>
+      <em title="Age"> ({props.entity.age})</em><br />
+      <span>HP: {props.entity.hp}/{props.entity.maxHp}</span>
+      <progress
+        title="HP"
+        max={props.entity.maxHp}
+        value={props.entity.hp}
+        percent={~~(props.entity.hp * 100 / props.entity.maxHp)}
+      >
       </progress>
     </div>
   );
