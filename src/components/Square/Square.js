1ffaabbe85795891823feb2cd637000bf2efd9ec
sEver <sEver@users.noreply.github.com>
2019-10-06 23:49:44 +0200

    Rewrite Square to PureComponent.
    This solves re-rendering of all the squares problem.

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import "./Square.css";
-export default function Square(props) {
-  console.log("Rendering Square");
-  return (
-    <button 
-      className="square" 
-      onClick={() => props.onClick(props.squareId)}
-    >
-      {props.value}
-    </button>
-  );
+
+class Square extends PureComponent {
+  /*
+    We use PureComponent, so it compares new props with previous props,
+    and only re-renders when props changed.
+  */
+  render() {
+    console.log("Rendering Square");
+    return (
+      <button className="square" onClick={() => this.props.onClick(this.props.squareId)}>
+        {this.props.value}
+      </button>
+    );
+  }
 }
+
+export default Square;
