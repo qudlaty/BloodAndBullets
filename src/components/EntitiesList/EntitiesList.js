5bf7ff95f907a3b52de8c39454609230f5b438a6
sEver <sEver@users.noreply.github.com>
2019-10-06 21:19:35 +0200

    Add render counters to board and list

--- src/components/EntitiesList/EntitiesList.js
+++ src/components/EntitiesList/EntitiesList.js
@@ -3,7 +3,9 @@ import EntityView from '../EntityView';
 import './EntitiesList.css';
 
 export default class ListOfEntities extends React.Component {
+  renderCounter = 0
   render() {
+    console.log("Rendering EntitiesList #", this.renderCounter++);
     var entities = this.props.entities.map(obj => {
       return (
         <EntityView entity={obj} key={obj.name}/>
