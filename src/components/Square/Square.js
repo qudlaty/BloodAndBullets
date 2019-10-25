188760cfabdf00bd65d20f6b2d4c0177705d22e9
sEver <sEver@users.noreply.github.com>
2019-10-26 01:16:52 +0200

    Add basic shooting animation

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -18,17 +18,53 @@ class Square extends React.PureComponent {
       } else {
         className += " dead";
       }
+      if(this.props.isShooting) {
+        className += " shooting";
+      }
     }
 
     let randomTime = `${(Math.random()+0.5).toFixed(2)}s`
     let animationBreathing = this.props.isBreathing ?
         `breathing ${randomTime} alternate infinite` : 'none';
 
+    let targetCoords = {x: 1, y: 1};
+    let customStyle = `
+      @keyframes shooting {
+        0%   {transform: scale(1);}
+        100% {transform: translate(${3*35*targetCoords.x}px, ${3*36*targetCoords.y}px);}
+      }
+
+      `;
+    let projectileNumber = 5;
+    while(projectileNumber--) {
+      customStyle += `
+      .projectile${projectileNumber} {
+        position: absolute;
+        top: 16px;
+        left: 16px;
+        animation: shooting 0.5s linear infinite;
+        animation-delay: ${projectileNumber  * 0.5}s;
+        color: white;
+      }`;
+    };
+    let projectile = this.props.isShooting ? "*" : "";
+    let projectiles = [];
+    projectileNumber = 5;
+
+    while(projectileNumber--) {
+      let className=`projectile projectile${projectileNumber}`
+      projectiles.push(<div class={className}>{projectile}</div>);
+    };
+
     return (
       <button className={className} onClick={() => this.props.onClick(this.props.squareId)}>
         <div className="content" style={{
           animation: animationBreathing
         }}>{this.props.value}</div>
+        <style>
+            {customStyle}
+        </style>
+        {projectiles}
       </button>
     );
   }
