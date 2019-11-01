162081e6364cee12882b09e8e7cc4eab81465308
sEver <sEver@users.noreply.github.com>
2019-11-01 01:25:11 +0100

    Switch main style to scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Game from './components/Game';
 
-import './style.css';
+import './style.scss';
 
 render(<Game />, document.getElementById('root'));
