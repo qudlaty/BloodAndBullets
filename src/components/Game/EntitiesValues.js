020ef3ff4a544ed58f93d625ef2ad8244d3de761
sEver <sEver@users.noreply.github.com>
2019-11-03 18:03:56 +0100

    Restructurize entities

--- src/components/Game/EntitiesValues.js
+++ src/components/Game/EntitiesValues.js
@@ -1,21 +1,45 @@
 const entities = [
   {
-    position: {x:0, y:0}, value: "😠", name: "John Rambo", age: 40, hp: 95, maxHp: 100,
-    inventory: ['KA-BAR', 'M16'], equipment: {head: 'Red Bandana'}, isBreathing: true,
+    name: "John Rambo", age: 40, hp: 95, maxHp: 100,
+    icon: "😠",
+    isBreathing: true,
+    position: {x:0, y:0},
+    inventory: ['KA-BAR', 'M16'],
+    equipment: {head: 'Red Bandana'},
     damage: 1, rounds: 10, maxRounds: 15, hasWeapon: true,
   },
   {
-    position: {x:0, y:1}, value: "👩", name: "Ellen Ripley", age: 30, hp: 50, maxHp: 65,
-    inventory: ['Lazer', 'Motion Detector'], equipment: {head: 'Afro'}, isBreathing: true,
+    name: "Ellen Replay", age: 30, hp: 50, maxHp: 65,
+    icon: "👩",
+    isBreathing: true,
+    position: {x:0, y:1},
+    inventory: ['Lazer', 'Motion Detector'],
+    equipment: {head: 'Afro'},
     damage: 1, rounds: 10, maxRounds: 10, hasWeapon: true,
   },
   {
-    position: {x:4, y:8}, value: "🧑🏻", name: "Lazer Blady", age: 60, hp: 75, maxHp: 100,
-    inventory: ['Lazer'], equipment: {}, isBreathing: true,
+    name: "Lazer Blady", age: 60, hp: 75, maxHp: 100,
+    icon: "🧑🏻",
+    isBreathing: true,
+    position: {x:4, y:8},
+    inventory: ['Lazer'],
+    equipment: {},
     damage: 10, rounds: 3, maxRounds: 3, hasWeapon: true,
   },
-  {position: {x:8, y:8}, value: "🐙", name: "Octo", age: 8, hp: 888, maxHp: 1000, inventory: [], equipment: {}, isBreathing: true,},
-  {position: {x:5, y:5}, value: "🦑", name: "Squid", age: 5, hp: 55, maxHp: 100, inventory: [], equipment: {}, isBreathing: true,},
+  {
+    name: "Octo", age: 8, hp: 888, maxHp: 1000,
+    icon: "🐙",
+    isBreathing: true,
+    position: {x:8, y:8},
+    inventory: [], equipment: {},
+  },
+  {
+    name: "Squid", age: 5, hp: 55, maxHp: 100,
+    icon: "🦑",
+    isBreathing: true,
+    position: {x:5, y:5},
+    inventory: [], equipment: {},
+  },
 
 ];
 
