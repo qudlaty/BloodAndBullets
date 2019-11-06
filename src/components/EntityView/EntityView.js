bb20d750ead43c202f0e52ef62769cf05145220d
sEver <sEver@users.noreply.github.com>
2019-11-06 23:59:34 +0100

    Simplify EntityView code

--- src/components/EntityView/EntityView.js
+++ src/components/EntityView/EntityView.js
@@ -8,44 +8,38 @@ class EntityView extends React.Component {
 
   render() {
     // console.log("Rendering EntityView. #", this.renderCount++);
-
+    let { entity } = this.props;
     /*
       FIXME: Below should be separated into several sub-components
       Each sub-component should receive flat data (position, hp, ...)
      */
     let className = "entity";
-    if(this.props.entity.active) {
+    if(entity.active) {
       className+=" active";
     }
 
-    let { entity } = this.props;
-    // let { position, value, age, name, hp, maxHp, isBreathing, active } = entity;
     let { isBreathing } = entity;
-    // let { x, y } = position;
-    // console.log(this.props)
-    // console.log(entity)
-    // console.log(isBreathing)
     let lifeSigns = isBreathing ? "ALIVE" : "DEAD";
     return (
 
       <div className={className}>
         <div className="position" title="Position">
-          <span>[{entity.position.x}, {this.props.entity.position.y}]</span>
+          <span>[{entity.position.x}, {entity.position.y}]</span>
         </div>
         <div className="portrait">
-          {this.props.entity.icon}
+          {entity.icon}
         </div>
-        <strong title="Name">{this.props.entity.name}</strong>
-        <em title="Age"> ({this.props.entity.age})</em>
+        <strong title="Name">{entity.name}</strong>
+        <em title="Age"> ({entity.age})</em>
         {` `}{lifeSigns}
         <br />
-        <span>HP: {this.props.entity.hp}/{this.props.entity.maxHp}</span>
-        <span> Rounds: {this.props.entity.rounds} / {this.props.entity.maxRounds}</span>
+        <span>HP: {entity.hp}/{entity.maxHp}</span>
+        <span> Rounds: {entity.rounds} / {entity.maxRounds}</span>
         <progress
           title="HP"
-          max={this.props.entity.maxHp}
-          value={this.props.entity.hp}
-          percent={~~(this.props.entity.hp * 100 / this.props.entity.maxHp)}
+          max={entity.maxHp}
+          value={entity.hp}
+          percent={~~(entity.hp * 100 / entity.maxHp)}
         >
         </progress>
       </div>
