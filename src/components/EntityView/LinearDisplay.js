11c968d227bfdf16544eced1725a375ed5952b86
sEver <sEver@users.noreply.github.com>
2019-11-07 01:31:44 +0100

    Drop progress bar in favor of manual div-based approach

---
+++ src/components/EntityView/LinearDisplay.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LinearDisplay.scss';
+export default function LinearDisplay(props) {
+  let className=`linear-display ${props.className}`
+  let percentage = ~~(props.current * 100 / props.max);
+  let progressStyle = {
+    width: `${percentage}%`,
+  }
+  let title = props.title || `${props.current}/${props.max}`;
+  return (
+    <div className={className}>
+      <div className="linear-display__label">{props.label}:&nbsp;</div>
+      <div className="linear-display__bar-container"
+        title={title}
+      >
+        <div className="linear-display__bar-progress" style={progressStyle}>
+          <span className="linear-display__bar-progress-text">{props.current}&nbsp;</span>
+        </div>
+      </div>
+    </div>
+  );
+}
