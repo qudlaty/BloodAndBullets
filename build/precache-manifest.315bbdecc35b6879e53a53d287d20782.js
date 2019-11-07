3df2b3a5b86c21a1d3f138eea663f9035ad15870
sEver <sEver@users.noreply.github.com>
2019-11-07 23:00:42 +0100

    Nighly Build 2019-11-07_1

--- build/precache-manifest.315bbdecc35b6879e53a53d287d20782.js
+++ build/precache-manifest.315bbdecc35b6879e53a53d287d20782.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f469a3fbb7fae33622554070081cb16d",
+    "revision": "406128d7521962f9698460d0082a0979",
     "url": "/BloodAndBulllets/build/index.html"
   },
   {
-    "revision": "02ae4657ee11804156c5",
-    "url": "/BloodAndBulllets/build/static/css/main.57570f3f.chunk.css"
+    "revision": "3ac75b86cdf7915d26a8",
+    "url": "/BloodAndBulllets/build/static/css/main.d1828058.chunk.css"
   },
   {
     "revision": "1a6549b0e185f3bb0f10",
     "url": "/BloodAndBulllets/build/static/js/2.0441643b.chunk.js"
   },
   {
-    "revision": "02ae4657ee11804156c5",
-    "url": "/BloodAndBulllets/build/static/js/main.7ff47e2e.chunk.js"
+    "revision": "3ac75b86cdf7915d26a8",
+    "url": "/BloodAndBulllets/build/static/js/main.749759dc.chunk.js"
   },
   {
     "revision": "5606cf87970be9941543",
