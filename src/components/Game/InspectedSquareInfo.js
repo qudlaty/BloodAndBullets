e7661e82e35c803fe3bb2975b1de34d6194ac844
sEver <sEver@users.noreply.github.com>
2019-11-17 03:06:25 +0100

    Show distance info to squareInspector only when something is selected

--- src/components/Game/InspectedSquareInfo.js
+++ src/components/Game/InspectedSquareInfo.js
@@ -7,23 +7,35 @@ export default class InspectedSquareInfo extends React.Component {
     render(){
         //this.props.squareNumber
         let inspectedSquare = this.props.squares[this.props.squareNumber];
+        let selected = this.props.selected;
+
         if(!inspectedSquare) {
             return <span>Nothing</span>
         }
         let squarePosition = SquaresService.targetSquarePosition(this.props.squareNumber);
-        let distanceToSelected = Helpers.calculateDistance(squarePosition.x, squarePosition.y);
+
+        let distanceInfo = '';
+        if(selected) {
+          let distanceToSelected = Helpers.calculateDistance(
+            squarePosition.x - selected.position.x,
+            squarePosition.y - selected.position.y
+          );
+          distanceInfo = (
+            <li>
+              Distance to selected: {~~distanceToSelected}
+            </li>
+          );
+        }
 
         return (
         <div>
             <h3>Square Info</h3>
             <ul>
+                {distanceInfo}
                 <li>
-                    Distance to selected: {~~distanceToSelected}
+                    Blood amount: {~~inspectedSquare.blood}
                 </li>
-                <li>
-                    Blood amount:{inspectedSquare.blood}
-                </li>
-            </ul> 
+            </ul>
         </div>
         );
     }
