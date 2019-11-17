809a74325a62a8009e1b820a9fd6c72fe2e40c4f
qudlaty <qudlaty@users.noreply.github.com>
2019-11-17 20:55:35 +0100

    add reload button

--- src/components/EntityCard/InventoryList.js
+++ src/components/EntityCard/InventoryList.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './InventoryList.scss';
 export default function InventoryList(props) {
 
-let className=`inventory-list ${props.className || ''}`
+let className = `inventory-list ${props.className || ''}`
+
 if(!props.inventory) {return null}
+let reloadButton;
 let inventoryItems = props.inventory.map(item => {
-    return (
-        <div onClick={() => props.onClick(item.name || item)} key={item.name} className="inventory-list__item">
-            <span>{item.name || item}</span>
-        </div>
-    )
+  if(item.reload) {
+    reloadButton = <button onClick={item.reload}>Reload</button>
+  }
+  return (
+    <div>
+      <div onClick={() => props.onClick(item.name || item)} key={item.name} className="inventory-list__item">
+        <span>{item.name || item}</span>
+      </div>
+      {reloadButton}
+    </div>
+  )
 });
 
   return (
