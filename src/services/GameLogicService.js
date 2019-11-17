66039ea7f3f5b33a1888058ca588ba92970df5e3
sEver <sEver@users.noreply.github.com>
2019-11-17 03:07:01 +0100

    Add basic GameLogic and GameModel services

---
+++ src/services/GameLogicService.js
@@ -0,0 +1,12 @@
+import GameModel from './GameModelService';
+
+class GameLogicClass {
+  run = () => {
+    console.log("Running Game Logic");
+    GameModel.access();
+    console.log(GameModel.entities);
+  }
+}
+
+const GameLogic = new GameLogicClass();
+export default GameLogic;
