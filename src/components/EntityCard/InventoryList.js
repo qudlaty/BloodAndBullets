d2697e796526ef4ea7506ed2da1012f10c6ad7ba
sEver <sEver@users.noreply.github.com>
2019-11-17 23:11:44 +0100

    Add reload-button color change depending on ammo status

--- src/components/EntityCard/InventoryList.js
+++ src/components/EntityCard/InventoryList.js
@@ -2,25 +2,39 @@ import React from 'react';
 import './InventoryList.scss';
 export default function InventoryList(props) {
 
-let className = `inventory-list ${props.className || ''}`
+  let className = `inventory-list ${props.className || ''}`
 
-if(!props.inventory) {return null}
-let reloadButton;
-let inventoryItems = props.inventory.map(item => {
-  if(item.reload) {
-    reloadButton = <button className="inventory-list__reload-button" onClick={item.reload}>Reload</button>
-  } else {
-    reloadButton = null;
-  }
-  return (
-    <div key={item.name}>
-      <div onClick={() => props.onClick(item.name || item)} key={item.name} className="inventory-list__item">
-        <span>{item.name || item}</span>
+  if(!props.inventory) {return null}
+  let reloadButton;
+  let inventoryItems = props.inventory.map(item => {
+    if(item.reload) {// has reload capability
+      let className = " inventory-list__reload-button ";
+
+      if(item.rounds === 0 || item.rounds === 'empty') {
+        className += " inventory-list__reload-button--empty "
+      } else if (item.rounds < item.maxRounds) {
+        className += " inventory-list__reload-button--partial "
+      }
+      reloadButton =
+        <button
+          className={className}
+          onClick={() => {
+            item.reload();
+            props.onClick(item.name);// this only to trigger the render
+          }}
+        >Reload</button>
+    } else {
+      reloadButton = null;
+    }
+    return (
+      <div key={item.name}>
+        <div onClick={() => props.onClick(item.name || item)} key={item.name} className="inventory-list__item">
+          <span>{item.name || item}</span>
+        </div>
+        {reloadButton}
       </div>
-      {reloadButton}
-    </div>
-  )
-});
+    )
+  });
 
   return (
     <div className={className}>
