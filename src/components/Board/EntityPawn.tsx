94d5589820834bab3373084d40dbcdfe75561d0b
qudlaty <qudlaty@users.noreply.github.com>
2019-12-22 18:44:55 +0100

    add breathing issue

--- src/components/Board/EntityPawn.tsx
+++ src/components/Board/EntityPawn.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { SquaresService } from "../../services";
 import { Entity } from "../../services/EntitiesValues";
+import "./EntityPawn.scss";
 
 interface EntityPawnProps {
   entity: Entity;
@@ -11,22 +12,24 @@ export default function EntityPawn(props: EntityPawnProps): ReactElement {
   let square = SquaresService.getSquare(entity.position.x, entity.position.y);
 
   let squareDistance = 38;
-  let className = " entity-piece ";
+  let className = " entity-pawn ";
 
   if (square.isLit) className += " is-lit ";
   if (square.isInTwilightZone) {
     className += " is-in-twilight-zone ";
   }
+
+  let randomTime = (Math.random() + 0.5).toFixed(2);
+  let animationBreathing = props.entity.isBreathing ? `breathing ${randomTime} alternate infinite linear` : "none";
+
   return (
     <div
       key={entity.name}
       className={className}
       style={{
+        animation: animationBreathing,
         left: squareDistance / 2 - 4 + squareDistance * entity.position.x,
         top: squareDistance / 2 - 4 + squareDistance * entity.position.y,
-        position: "absolute",
-        fontSize: "1.5em",
-        transition: "all 0.5s",
       }}
     >
       {entity.icon}
