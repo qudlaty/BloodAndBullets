8e3e8048f1e4cbdc688e8298c077a80422926ba1
sEver <sEver@users.noreply.github.com>
2019-12-25 20:04:13 +0100

    Document Helpers.ts

--- src/helpers/Helpers.ts
+++ src/helpers/Helpers.ts
@@ -1,6 +1,7 @@
 import { Entity } from "../services/EntitiesValues";
 import { Square } from "../services/SquaresService";
 
+/** Takes a collection and a list of strings describing object keys. Sets all given fields on a collection to `undefined` */
 export function resetGivenFieldsOnACollection(collection, ...fieldNames) {
   collection.forEach((item) => {
     fieldNames.forEach((fieldName) => {
@@ -9,16 +10,19 @@ export function resetGivenFieldsOnACollection(collection, ...fieldNames) {
   });
 }
 
+/** Takes a number and fits it within given boundaries, then returns it */
 export function getNumberWithinBoundaries(value: number, min: number, max: number): number {
   if (value < min) value = min;
   if (value > max) value = max;
   return value;
 }
 
+/** Returns an integer between min and max */
 export function getRandomIntInclusive(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/** Returns angle (in degrees) of a given vector */
 export function calculateAngle(x: number, y: number): number {
   let angle: number;
   if (y >= 0) {
@@ -29,10 +33,12 @@ export function calculateAngle(x: number, y: number): number {
   return angle;
 }
 
+/** Pitagorean distance between 0,0 and given coords */
 export function calculateDistance(x: number, y: number): number {
   return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
+/** Mixing classess together */
 export function applyMixins(derivedCtor: any, baseCtors: any[]) {
   // see https://www.typescriptlang.org/docs/handbook/mixins.html
   baseCtors.forEach((baseCtor) => {
@@ -42,6 +48,7 @@ export function applyMixins(derivedCtor: any, baseCtors: any[]) {
   });
 }
 
+/** Checking if selected entity is on a targeted square */
 export function isSelectedTargeted(selected: Entity, targeted: Square): boolean {
   if (selected && targeted && targeted.entity && selected.name === targeted.entity.name) {
     return true;
