53598ce5a9d012de89768cde765ba30ec7ceb033
sEver <sEver@users.noreply.github.com>
2020-02-19 02:15:54 +0100

    Add auto-scrolling to message box

--- src/components/Game/MessageBox.tsx
+++ src/components/Game/MessageBox.tsx
@@ -3,6 +3,12 @@ import MessageService from "../../services/MessageService";
 
 export class MessageBox extends React.Component {
   messages = MessageService.messages;
+  messageNumber;
+
+  shouldComponentUpdate() {
+    // don't update if there are no new messages since last render
+    return this.messages.length != this.messageNumber;
+  }
 
   renderMessage = (message: string, number: number) => {
     return (
@@ -13,8 +19,25 @@ export class MessageBox extends React.Component {
   };
 
   render() {
+    if (!this.messages.length) return null;
     let RenderedMessages = this.messages.map((message, number) => this.renderMessage(message, number));
+    // remember the number of messages rendered
+    this.messageNumber = this.messages.length;
+    return (
+      <ul className="messages" id="messages">
+        {RenderedMessages}
+      </ul>
+    );
+  }
 
-    return <ul className="messagesUl">{RenderedMessages}</ul>;
+  scrollElement() {
+    window.requestAnimationFrame(() => {
+      var element = document.getElementById("messages");
+      element.scrollTop = element.scrollHeight;
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.messages.length) this.scrollElement();
   }
 }
