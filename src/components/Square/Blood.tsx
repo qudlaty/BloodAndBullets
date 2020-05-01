2883ea8f13825ce180b061ba01955c218929337a
sEver <sEver@users.noreply.github.com>
2020-05-01 20:03:13 +0200

    Add descriptions to Square and subcomponents

--- src/components/Square/Blood.tsx
+++ src/components/Square/Blood.tsx
@@ -4,6 +4,13 @@ interface BloodProps {
   bloodAmount: number; // receives amount of blood ONLY
 }
 
+/**
+ * @description
+ * Visualises blood by adding red background
+ * and a little number in the corner.
+ *
+ * @param bloodAmount - number representing the amount of blood.
+ */
 class Blood extends React.PureComponent<BloodProps> {
   render() {
     let { bloodAmount } = this.props;
