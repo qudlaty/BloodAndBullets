a77abcd496dcb88907a459031947a107985bafb7
sEver <sEver@users.noreply.github.com>
2021-04-07 21:44:02 +0200

    Add proper hpBars

---
+++ src/components/EntityPawn/HpBar.tsx
@@ -0,0 +1,30 @@
+import { timeStamp } from "console";
+import React from "react";
+import "./HpBar.scss";
+
+interface HpBarProps {
+    current: number;
+    max: number;
+    color: string;
+}
+
+export default class HpBar extends React.Component<HpBarProps> {
+
+    render() {
+        let percentage = ~~((this.props.current * 100) / this.props.max);
+        let progressStyle = {
+            width: `${percentage}%`,
+            backgroundColor: this.props.color
+        };
+        return(
+            <div className="hpbar">
+                <div className="hpbar__indicator" style={progressStyle}>
+
+                </div>
+                <span className="hpbar__text">
+                        {this.props.current}
+                </span>
+            </div>
+        );
+    }
+}
