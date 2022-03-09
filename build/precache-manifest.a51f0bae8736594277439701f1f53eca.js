3e11458784bf2e7854bb97005da20d6680d7a600
sEver <sEver@users.noreply.github.com>
2022-03-09 04:02:35 +0100

    Nightly Build 2022-03-09

--- build/precache-manifest.a51f0bae8736594277439701f1f53eca.js
+++ build/precache-manifest.a51f0bae8736594277439701f1f53eca.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8ba2148810363e46287067f2c40107a",
+    "revision": "57717461440145da3e2d9ff92ff2dfea",
     "url": "/BloodAndBullets/build/index.html"
   },
   {
-    "revision": "487f5abce9c2d0dde953",
-    "url": "/BloodAndBullets/build/static/css/main.a0c9653e.chunk.css"
+    "revision": "fca92a80c16be5440a36",
+    "url": "/BloodAndBullets/build/static/css/main.51199576.chunk.css"
   },
   {
     "revision": "262f7e737b7ed714f571",
     "url": "/BloodAndBullets/build/static/js/2.2f38c7e9.chunk.js"
   },
   {
-    "revision": "487f5abce9c2d0dde953",
-    "url": "/BloodAndBullets/build/static/js/main.e5ef431e.chunk.js"
+    "revision": "fca92a80c16be5440a36",
+    "url": "/BloodAndBullets/build/static/js/main.c8eb4476.chunk.js"
   },
   {
     "revision": "7f1c8ed36e2da6a12338",
