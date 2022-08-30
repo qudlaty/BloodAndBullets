1b44b9e8981195dd1a44bf604b73059d30dd61e2
sEver <sEver@users.noreply.github.com>
2022-08-30 16:19:39 +0200

    Nightly Build 2022-08-30

---
+++ build/precache-manifest.256d0f5e21e5d85b08cb00c9c53bccd7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354486cc6e537353c03d7788afe747a5",
+    "url": "/BloodAndBullets/build/index.html"
+  },
+  {
+    "revision": "3013c1fb3f72ae9dbe20",
+    "url": "/BloodAndBullets/build/static/css/main.acfc1589.chunk.css"
+  },
+  {
+    "revision": "a0b8b9990ad52d41360b",
+    "url": "/BloodAndBullets/build/static/js/2.f31d5926.chunk.js"
+  },
+  {
+    "revision": "3013c1fb3f72ae9dbe20",
+    "url": "/BloodAndBullets/build/static/js/main.e74e2fda.chunk.js"
+  },
+  {
+    "revision": "7f1c8ed36e2da6a12338",
+    "url": "/BloodAndBullets/build/static/js/runtime-main.561fb24a.js"
+  },
+  {
+    "revision": "aadaacb745c13dc80df2570f55c8018f",
+    "url": "/BloodAndBullets/build/static/media/ant_whatsapp_1f41c.aadaacb7.png"
+  },
+  {
+    "revision": "c3bff1bb59b6adeea31e567b99167eb2",
+    "url": "/BloodAndBullets/build/static/media/astronaut-light-skin-tone_joy_pixels_1f9d1-1f3fb-200d-1f680.c3bff1bb.png"
+  },
+  {
+    "revision": "3e8fdf7a802068c515ff6123d510090d",
+    "url": "/BloodAndBullets/build/static/media/microbe__joypixels_1f9a0.3e8fdf7a.png"
+  },
+  {
+    "revision": "ef52dfbdeeba20cb147018fa73bed421",
+    "url": "/BloodAndBullets/build/static/media/mosquito__emojipedia_1f99f.ef52dfbd.png"
+  },
+  {
+    "revision": "657d548f35193f108ba35d69def296ec",
+    "url": "/BloodAndBullets/build/static/media/oil-drum_red_emojidex_1f6e2.657d548f.png"
+  },
+  {
+    "revision": "59751a45f7e7fd9e1b4a33306881d9dd",
+    "url": "/BloodAndBullets/build/static/media/robot-face__emojidex1f916.59751a45.png"
+  },
+  {
+    "revision": "64049333796894e05c6a8d126ef410d9",
+    "url": "/BloodAndBullets/build/static/media/spider__whatsapp_1f577-fe0f.64049333.png"
+  },
+  {
+    "revision": "ddab14f8240c5a5b09540dc300cab9c1",
+    "url": "/BloodAndBullets/build/static/media/squid__joypixels_1f991.ddab14f8.png"
+  }
+]);
