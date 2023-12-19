f1921e62a47f1cc2aa2042046f0d510448db5257
sEver <sEver@users.noreply.github.com>
2023-12-19 13:47:17 +0100

    Move GameInfo into SlideInPanel

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { EntitiesService, GameModel, GameActionsClassForGameComponent, GameState, Entity } from "services";
 
 // components
-import { Board, MessageBox, Controls } from "components";
+import { Board, MessageBox, Controls, SlideInPanel } from "components";
 import TargetedSquareInfo from "./TargetedSquareInfo";
 import SelectedEntityInfo from "./SelectedEntityInfo";
 
@@ -48,7 +48,7 @@ export class Game extends React.PureComponent<void, GameState> {
       return result;
     };
     return EntitiesService.entities
-      .filter((entity) => entity.isFriendly)
+      .filter(entity => entity.isFriendly)
       .reduce(
         apAdder, // takes a previous value and an array element (entity), returns number (next value)
         startValue // start value for only the first iteration
@@ -63,7 +63,7 @@ export class Game extends React.PureComponent<void, GameState> {
           <Board
             squares={this.state.squares}
             entities={this.state.entities}
-            onClick={(i) => GameActions.handleClickV2(i)}
+            onClick={i => GameActions.handleClickV2(i)}
             size={this.state.arenaSize}
             isRotated={this.state.isBoardRotated}
             style={{ fontSize: `${this.state.squareSize}px` }}
@@ -73,77 +73,78 @@ export class Game extends React.PureComponent<void, GameState> {
             onKeyPress={GameActions.handleKeyPress}
           ></Controls>
         </div>
+        <SlideInPanel>
+          <div className={styles.game__info}>
+            <div className={styles.actions}>
+              <div className={styles.action_row}>
+                <button onClick={GameActions.toggleEditorMode}>Editor Mode</button>
+                <button onClick={GameModel.saveMap}>Save Map</button>
+                <button onClick={() => GameModel.loadMap(GameActions)}>Load Map</button>
+                <button onClick={() => GameModel.getMap()}>Generate MAP</button>
+              </div>
 
-        <div className={styles.game__info}>
-          <div className={styles.actions}>
-            <div className={styles.action_row}>
-              <button onClick={GameActions.toggleEditorMode}>Editor Mode</button>
-              <button onClick={GameModel.saveMap}>Save Map</button>
-              <button onClick={() => GameModel.loadMap(GameActions)}>Load Map</button>
-              <button onClick={() => GameModel.getMap()}>Generate MAP</button>
+              <div className={styles.action_row}>
+                <button onClick={GameActions.toggleRotateBoard}>Rotate Board</button>
+                <button onClick={GameActions.zoomIn}>Zoom In (+)</button>
+                <button onClick={GameActions.zoomOut}>Zoom Out (-)</button>
+              </div>
+              <div className={styles.action_row}>
+                <button onClick={() => GameActions.nuke(40)} className={styles["button-nuke"]}>
+                  Nuke All
+                </button>
+                <button onClick={GameActions.ceaseFire}>Cease Fire</button>
+                <button onClick={GameActions.executeActions} className={styles.execute_actions}>
+                  Execute Actions
+                </button>
+              </div>
+
+              <div className={styles.action_row}>
+                <span className={styles["step-counter"]}>Tick: {this.stepNumber}</span>
+
+                <label className={` ${styles.button} ${styles["auto-cycle"]}`}>
+                  <input type="checkbox" checked={this.state.isAutoLoopOn} onChange={GameActions.switchAutoLoop} />
+                  <span>Auto Cycle</span>
+                </label>
+
+                <button onClick={GameActions.nextTick} className={styles.button + " " + styles.next_tick}>
+                  Next Tick
+                </button>
+              </div>
+
+              <div className={styles.action_row}>
+                <span>
+                  Enemies alive: {this.state.enemiesAlive} <br />
+                  Friends alive: {this.state.friendsAlive} <br />
+                  {this.state.enemiesAlive && !this.state.friendsAlive ? "YOU LOST" : ""}
+                  {this.state.friendsAlive && !this.state.enemiesAlive ? "YOU WON" : ""}
+                </span>
+                &nbsp;
+                <p className="instructions"></p>
+                <span>Friends actions points: {this.calculateFriendlyActionPoints()} </span>
+              </div>
             </div>
+            <div className={styles["interaction-container"]}>
+              <SelectedEntityInfo
+                selected={this.state.selected}
+                handleDeselectAllEntities={GameActions.handleDeselectAllEntities}
+                onInventoryClick={GameActions.onInventoryClick}
+                processInterface={() => GameActions.processInterface()}
+              />
 
-            <div className={styles.action_row}>
-              <button onClick={GameActions.toggleRotateBoard}>Rotate Board</button>
-              <button onClick={GameActions.zoomIn}>Zoom In (+)</button>
-              <button onClick={GameActions.zoomOut}>Zoom Out (-)</button>
+              <TargetedSquareInfo
+                className={styles.targeted}
+                squareNumber={this.state.targetedSquareNumber}
+                squares={this.state.squares}
+                selected={this.state.selected}
+                targeted={this.state.targeted}
+                onInventoryClick={GameActions.onInventoryClick}
+                processInterface={() => GameActions.processInterface()}
+              />
+              <div></div>
             </div>
-            <div className={styles.action_row}>
-              <button onClick={() => GameActions.nuke(40)} className={styles["button-nuke"]}>
-                Nuke All
-              </button>
-              <button onClick={GameActions.ceaseFire}>Cease Fire</button>
-              <button onClick={GameActions.executeActions} className={styles.execute_actions}>
-                Execute Actions
-              </button>
-            </div>
-
-            <div className={styles.action_row}>
-              <span className={styles["step-counter"]}>Tick: {this.stepNumber}</span>
-
-              <label className={` ${styles.button} ${styles["auto-cycle"]}`}>
-                <input type="checkbox" checked={this.state.isAutoLoopOn} onChange={GameActions.switchAutoLoop} />
-                <span>Auto Cycle</span>
-              </label>
-
-              <button onClick={GameActions.nextTick} className={styles.button + " " + styles.next_tick}>
-                Next Tick
-              </button>
-            </div>
-
-            <div className={styles.action_row}>
-              <span>
-                Enemies alive: {this.state.enemiesAlive} <br />
-                Friends alive: {this.state.friendsAlive} <br />
-                {this.state.enemiesAlive && !this.state.friendsAlive ? "YOU LOST" : ""}
-                {this.state.friendsAlive && !this.state.enemiesAlive ? "YOU WON" : ""}
-              </span>
-              &nbsp;
-              <p className="instructions"></p>
-              <span>Friends actions points: {this.calculateFriendlyActionPoints()} </span>
-            </div>
+            <MessageBox />
           </div>
-          <div className={styles["interaction-container"]}>
-            <SelectedEntityInfo
-              selected={this.state.selected}
-              handleDeselectAllEntities={GameActions.handleDeselectAllEntities}
-              onInventoryClick={GameActions.onInventoryClick}
-              processInterface={() => GameActions.processInterface()}
-            />
-
-            <TargetedSquareInfo
-              className={styles.targeted}
-              squareNumber={this.state.targetedSquareNumber}
-              squares={this.state.squares}
-              selected={this.state.selected}
-              targeted={this.state.targeted}
-              onInventoryClick={GameActions.onInventoryClick}
-              processInterface={() => GameActions.processInterface()}
-            />
-            <div></div>
-          </div>
-          <MessageBox />
-        </div>
+        </SlideInPanel>
       </div>
     );
   }
