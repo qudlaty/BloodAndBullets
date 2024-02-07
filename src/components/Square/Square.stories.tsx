711eab78e32594c17681eb0cb0cdc22e6ddcd722
sEver <sEver@users.noreply.github.com>
2024-02-07 02:00:44 +0100

    Refactor Square stories

--- src/components/Square/Square.stories.tsx
+++ src/components/Square/Square.stories.tsx
@@ -3,37 +3,23 @@ import { SquareComponent } from "components";
 import { action } from "@storybook/addon-actions";
 import React from "react";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof SquareComponent> = {
-  //title: "Square",
   component: SquareComponent,
-  parameters: {
-    backgrounds: {
-      default: "dark",
-    },
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     onClick: { action: "clicked" },
   },
+  render: args => (
+    <>
+      <div style={fontSize40px}>
+        <SquareComponent {...args}></SquareComponent>
+      </div>
+    </>
+  ),
 }; // satisfies Meta<typeof SquareComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-/*
-squareId: number;
-className?: string;
-onClick: (squareIndex: number) => void;
-blood: number;
-items: Item[]; // collection of objects
-itemsNumber: number; // here to trigger update when list length changes
-*/
 const fontSize40px = { fontSize: "40px" };
 
 export const Empty: Story = {
@@ -60,29 +46,14 @@ export const Heavy: Story = {
     items: [],
     itemsNumber: 5,
   },
-  render: args => (
-    <>
-      <div style={fontSize40px}>
-        <SquareComponent {...args}></SquareComponent>
-      </div>
-    </>
-  ),
 };
 
-export function SuperHeavy(): JSX.Element {
-  const args = {
+export const SuperHeavy: Story = {
+  args: {
     squareId: 0,
     onClick: action("clicked"),
     blood: 40,
     items: [],
     itemsNumber: 5,
-  };
-
-  return (
-    <>
-      <div style={fontSize40px}>
-        <SquareComponent {...args}></SquareComponent>
-      </div>
-    </>
-  );
-}
+  },
+};
