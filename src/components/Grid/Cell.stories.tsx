32151ad746c0cdcee130adc711f6f4b0b8712220
sEver <sEver@users.noreply.github.com>
2024-02-07 02:07:26 +0100

    Clean up stories for Cell

--- src/components/Grid/Cell.stories.tsx
+++ src/components/Grid/Cell.stories.tsx
@@ -1,13 +1,11 @@
+import type { Meta, StoryObj } from "@storybook/react";
 import { Cell } from "components";
 
-// START OF UGLY STORYBOOK SUGAR
-import type { Meta, StoryObj } from "@storybook/react";
 const meta = {
   component: Cell,
 } satisfies Meta<typeof Cell>;
 export default meta;
 type Story = StoryObj<typeof meta>;
-// END OF UGLY STORYBOOK SUGAR
 
 export const Empty: Story = {
   args: {
