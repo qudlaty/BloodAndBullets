89a2039913ff638af7cd6a0d1efeb871a9712255
sEver <sEver@users.noreply.github.com>
2024-02-07 02:06:15 +0100

    Clean up stories for Controls

--- src/components/Controls/Controls.stories.tsx
+++ src/components/Controls/Controls.stories.tsx
@@ -1,13 +1,11 @@
+import type { Meta, StoryObj } from "@storybook/react";
 import { Controls } from "components";
 
-// START OF UGLY STORYBOOK SUGAR
-import type { Meta, StoryObj } from "@storybook/react";
 const meta = {
   component: Controls,
 } satisfies Meta<typeof Controls>;
 export default meta;
 type Story = StoryObj<typeof meta>;
-// END OF UGLY STORYBOOK SUGAR
 
 export const Empty: Story = {
   args: {},
