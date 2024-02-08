64a3b4f7726b9e0c61132e7a99bc0f271339129b
sEver <sEver@users.noreply.github.com>
2024-02-08 01:01:45 +0100

    Enhance Board Stories

--- src/components/Board/Board.stories.tsx
+++ src/components/Board/Board.stories.tsx
@@ -1,17 +1,39 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
 import { Board } from "components";
 
 const meta = {
   component: Board,
+  render: args => (
+    <>
+      <div style={{ fontSize: "40px" }}>
+        <Board {...args}></Board>
+      </div>
+    </>
+  ),
+  args: {
+    squares: [],
+    entities: [],
+    size: 5,
+    isRotated: false,
+  },
 } satisfies Meta<typeof Board>;
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    squares: [],
-    entities: [],
     size: 10,
-    isRotated: false,
+  },
+};
+
+export const Three: Story = {
+  args: {
+    size: 3,
+  },
+};
+export const Twenty: Story = {
+  args: {
+    size: 20,
   },
 };
