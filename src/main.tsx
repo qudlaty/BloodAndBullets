04c7be504351102f857c50be6a37f376da37a56b
sEver <sEver@users.noreply.github.com>
2024-12-27 05:27:10 +0100

    Add 'DEFAULT' prefix to the default files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './DEFAULT-index.css'
+import App from './DEFAULT-App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
