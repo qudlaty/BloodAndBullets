f39c75229bf144c98f009ce031ddf09b5c14bd4b
sEver <sEver@users.noreply.github.com>
2024-12-27 06:06:28 +0100

    Add support for SCSS import paths starting in root dir of the project
    This will be needed because of reasons ^^

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     tsconfigPaths()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [path.resolve(__dirname, '.')],
+      },
+    },
+  },
 })
