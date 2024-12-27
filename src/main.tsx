613873504579e989a323633af901aca44b557f8a
sEver <sEver@users.noreply.github.com>
2024-12-27 05:32:54 +0100

    Make import paths start inside `./src`
    by adding both TS and Vite configs for that

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './DEFAULT-index.css'
-import App from './DEFAULT-App.tsx'
+import App from 'DEFAULT-App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
