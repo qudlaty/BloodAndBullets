2a502db5cffc4d1a729125019398d311bfdabf17
sEver <sEver@users.noreply.github.com>
2024-12-27 05:42:07 +0100

    Change css paths to absolute

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './DEFAULT-index.css'
+import 'DEFAULT-index.css'
 import App from 'DEFAULT-App.tsx'
 
 createRoot(document.getElementById('root')!).render(
