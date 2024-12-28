0739b9190c5b885e2fc073e2ab842870a258eda8
sEver <sEver@users.noreply.github.com>
2024-12-28 04:08:29 +0100

    Change build target directory for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths()
-  ],
+  plugins: [react(), tsconfigPaths()],
   css: {
     preprocessorOptions: {
       scss: {
-        loadPaths: [path.resolve(__dirname, '.')],
+        loadPaths: [path.resolve(__dirname, ".")],
       },
     },
   },
-})
+  build: {
+    outDir: "build",
+  },
+});
